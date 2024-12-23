7a1dd695eb7c55809986ad46304a7c8d397120b2
tetsuo <ogunduz@gmail.com>
2024-12-23 14:15:07 +0100

    Update logging timestamp; reduce precision

--- src/log.c
+++ src/log.c
@@ -7,19 +7,18 @@
 static void get_timestamp(char *buffer, size_t size)
 {
   struct timespec ts;
-  clock_gettime(CLOCK_REALTIME, &ts); // Use CLOCK_REALTIME for the current time
+  clock_gettime(CLOCK_REALTIME, &ts);
 
-  struct tm *local_time = localtime(&ts.tv_sec); // Convert seconds to local time
+  struct tm *local_time = localtime(&ts.tv_sec);
   if (local_time)
   {
-    snprintf(buffer, size, "%04d/%02d/%02d %02d:%02d:%02d.%03ld",
+    snprintf(buffer, size, "%04d/%02d/%02d %02d:%02d:%02d",
              local_time->tm_year + 1900,
              local_time->tm_mon + 1,
              local_time->tm_mday,
              local_time->tm_hour,
              local_time->tm_min,
-             local_time->tm_sec,
-             ts.tv_nsec / 1000000); // Convert nanoseconds to milliseconds
+             local_time->tm_sec);
   }
   else
   {
@@ -31,9 +30,9 @@ void log_printf(const char *format, ...)
 {
   va_list args;
   va_start(args, format);
-  char timestamp[25];
+  char timestamp[20];
   get_timestamp(timestamp, sizeof(timestamp));
-  fprintf(stderr, "%s  ", timestamp);
+  fprintf(stderr, "%s ", timestamp);
   vfprintf(stderr, format, args);
   fprintf(stderr, "\n");
   va_end(args);
